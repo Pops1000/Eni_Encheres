fce26aa5bc64bc23800f6430a0b042f8103a45fc
mygreenpanda <51167787+mygreenpanda@users.noreply.github.com>
2023-08-31 19:27:59 +0200

    Delete add_categories.sql

--- add_categories.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO categories (libelle) VALUES ('Informatique');
-INSERT INTO categories (libelle) VALUES ('Ameublement');
-INSERT INTO categories (libelle) VALUES ('Vêtement');
-INSERT INTO categories (libelle) VALUES ('Sport & Loisirs');
